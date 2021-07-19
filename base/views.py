aa07a284788f009dd55f99237a41070efb54e8e0
kastriot96 <79404857+kastriot96@users.noreply.github.com>
2021-07-19 21:53:38 +0200

    update

--- base/views.py
+++ base/views.py
@@ -1,6 +1,34 @@
+from .models import Task
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView , UpdateView , DeleteView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
 
 # Create your views here.
-def taskList(request):
-    return HttpResponse('To Do List')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+
+class TaskCreate(CreateView):
+    #we send post request , therefor we need to create an item
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task 
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
